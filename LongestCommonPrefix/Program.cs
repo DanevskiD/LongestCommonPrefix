8ddf3e1af433faa09fbb13fc129ac4b998fd0478
Denis <danevskidenis@gmail.com>
2023-11-01 23:24:00 +0100

    Add project files.

---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LongestCommonPrefix;
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.Length; i++)
+        {
+            string currentPrefix = string.Empty;
+
+            for (int j = 0; j < strs[i].Length; j++)
+            {
+                if (j >= prefix.Length)
+                    break;
+                if (strs[i][j] == prefix[j])
+                {
+                    currentPrefix += strs[i][j];
+                }
+                else
+                {
+                    break;
+                }
+            }
+            prefix = currentPrefix;
+        }
+        return prefix;
+    }
+}
